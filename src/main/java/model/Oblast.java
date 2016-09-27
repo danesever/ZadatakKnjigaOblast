cf9eafdf0d82e6a7d11a3ef76e99af76e0eb6310
danesever <bresa86@yahoo.com>
2016-09-27 20:31:58 +0200

    fixes #5 Uradjena klasa Oblast

--- src/main/java/model/Oblast.java
+++ src/main/java/model/Oblast.java
@@ -1,4 +1,48 @@
 package model;
 
+import com.j256.ormlite.field.DatabaseField;
+
 public class Oblast {
+    public static final String POLJE_NAZIV = "naziv";
+    public static final String POLJE_POCETNA_STRANA = "pocetna_strana";
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField(columnName = POLJE_NAZIV, canBeNull = false)
+    private String naziv;
+    @DatabaseField(columnName = POLJE_POCETNA_STRANA, canBeNull = false)
+    private int pocetnaStrana;
+
+    @DatabaseField(foreign = true,foreignAutoRefresh = true,canBeNull = false)
+    private Knjiga knjiga;
+
+    public Oblast(String naziv, int pocetnaStrana) {
+        this.naziv = naziv;
+        this.pocetnaStrana = pocetnaStrana;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getPocetnaStrana() {
+        return pocetnaStrana;
+    }
+
+    public void setPocetnaStrana(int pocetnaStrana) {
+        this.pocetnaStrana = pocetnaStrana;
+    }
+
+    @Override
+    public String toString() {
+        return "Oblast{" +
+                "id=" + id +
+                ", naziv='" + naziv + '\'' +
+                ", pocetnaStrana=" + pocetnaStrana +
+                '}';
+    }
 }
